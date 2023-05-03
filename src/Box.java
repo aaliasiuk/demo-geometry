abc9dd2d8b1a7ba05147f262ca66da1b0b98ccf4
Andrei Aliasiuk <aliasiuk13@gmail.com>
2023-05-03 10:53:19 -0700

    refactor and use constructor with params

--- src/Box.java
+++ src/Box.java
@@ -2,12 +2,14 @@ public class Box {
     double width;
     double height;
     double depth;
-Box(){
-    System.out.println("Box object constructor");
-    width=10;
-    height=10;
-    depth=10;
-}
+
+    Box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+
+    }
+
     double volume() {
         return width * height * depth;
     }
--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -1,7 +1,7 @@
 public class BoxDemo {
     public static void main(String args[]) {
-        Box mybox1 = new Box();
-        Box mybox2 = new Box();
+        Box mybox1 = new Box(10,20,15);
+        Box mybox2 = new Box(3,6,9);
         double vol;
 
 
