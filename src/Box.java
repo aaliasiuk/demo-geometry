3cc8ea8bba834cf881b4f28217e9dfc9b016a44e
Andrei Aliasiuk <aliasiuk13@gmail.com>
2023-05-03 10:47:35 -0700

    refactor and setDim method

--- src/Box.java
+++ src/Box.java
@@ -6,4 +6,9 @@ public class Box {
     double volume() {
         return width * height * depth;
     }
+    void setDim(double w,double h, double d){
+        width = w;
+        height = h;
+        depth = d;
+    }
 }
--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -5,13 +5,9 @@ public class BoxDemo {
         double vol;
 
 
-        mybox1.width = 10;
-        mybox1.height = 20;
-        mybox1.depth = 15;
+        mybox1.setDim(10,20,15);
+        mybox2.setDim(3,6,9);
 
-        mybox2.width = 3;
-        mybox2.height = 6;
-        mybox2.depth = 9;
 
         vol = mybox1.volume();
         System.out.println("Volume equals to " + vol);
