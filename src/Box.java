eb321573b3e39e38f60e0856f36c1eab5b965217
Andrei Aliasiuk <aliasiuk13@gmail.com>
2023-05-03 10:39:43 -0700

    refactor boxdemo class to use volume method

--- src/Box.java
+++ src/Box.java
@@ -2,4 +2,9 @@ public class Box {
     double width;
     double height;
     double depth;
+
+    void volume(){
+        System.out.print("Volume equals to ");
+        System.out.println(width * height *depth);
+    }
 }
--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -2,7 +2,7 @@ public class BoxDemo {
     public static void main(String args[]) {
         Box mybox1 = new Box();
         Box mybox2 = new Box();
-        double vol;
+
 
         mybox1.width = 10;
         mybox1.height = 20;
@@ -12,10 +12,7 @@ public class BoxDemo {
         mybox2.height = 6;
         mybox2.depth = 9;
 
-        vol = mybox1.width * mybox1.height * mybox1.depth;
-        System.out.println("Volume equals to " + vol);
-
-        vol = mybox2.width * mybox2.height * mybox2.depth;
-        System.out.println("Volume equals to " + vol);
+       mybox1.volume();
+       mybox2.volume();
     }
 }
